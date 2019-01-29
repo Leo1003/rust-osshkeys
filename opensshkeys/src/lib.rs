d8f27e9d759e09c1a92caef73081a1d920d6261b
Leo <leo881003@gmail.com>
2019-01-29 23:01:11 +0800

    Construct preliminary trait structure

--- opensshkeys/src/lib.rs
+++ opensshkeys/src/lib.rs
@@ -1,7 +1,25 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#[derive(Clone)]
+pub struct Error {}
+
+pub trait PublicKey {
+    fn size(&self) -> usize;
+    fn fingerprint(&self, algo: HashAlgo) -> Vec<u8>;
+    fn keytype(&self) -> &'static str;
+    fn verify(&self, data: &[u8]) -> Result<bool, Error>;
+    fn comment(&self) -> &String;
+    fn set_comment(&mut self, comment: &str) -> ();
+}
+
+pub trait PrivateKey: PublicKey {
+    fn sign(&self, data: &[u8]) -> Result<Vec<u8>, Error>;
+    fn as_public_key(&self) -> &PublicKey;
+}
+
+pub enum HashAlgo {
+    MD5,
+    SHA1,
+    SHA224,
+    SHA256,
+    SHA384,
+    SHA512,
 }
