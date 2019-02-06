a7afb4b1b5d5d6bc5da7dec160aab440cd5e9e33
Leo <leo881003@gmail.com>
2019-02-06 20:34:28 +0800

    Add buf reader

---
+++ opensshkeys/src/sshbuf.rs
@@ -0,0 +1,91 @@
+pub use openssl::bn::BigNum;
+use byteorder::{BigEndian, ReadBytesExt};
+use std::io;
+
+const MAX_BIGNUM: usize = 16384 / 8;
+
+pub struct SshReader<R> {
+    stream: R,
+}
+
+impl<R: io::Read> SshReader<R> {
+    pub fn new(stream: R) -> SshReader<R> {
+        SshReader {
+            stream: stream
+        }
+    }
+    pub fn read_bytes(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        self.stream.read(buf)
+    }
+    pub fn read_bool(&mut self) -> io::Result<bool> {
+        match self.stream.read_u8() {
+            Ok(n) => Ok(n != 0),
+            Err(e) => Err(e)
+        }
+    }
+    pub fn read_u32(&mut self) -> io::Result<u32> {
+        self.stream.read_u32::<BigEndian>()
+    }
+    pub fn read_u64(&mut self) -> io::Result<u64> {
+        self.stream.read_u64::<BigEndian>()
+    }
+    pub fn read_string(&mut self) -> io::Result<Vec<u8>> {
+        let length = match self.read_u32() {
+            Ok(n) => n as usize,
+            Err(e) => return Err(e)
+        };
+        let mut buf = vec![0u8; length];
+        match self.read_bytes(&mut buf) {
+            Ok(n) => {
+                if n == length {
+                    Ok(buf)
+                } else {
+                    Err(io::Error::new(io::ErrorKind::InvalidData, "Invalid length"))
+                }
+            },
+            Err(e) => Err(e)
+        }
+    }
+    pub fn read_utf8(&mut self) -> io::Result<String> {
+        match self.read_string() {
+            Ok(data) => {
+                match String::from_utf8(data) {
+                    Ok(string) => Ok(string),
+                    Err(_) => Err(io::Error::new(io::ErrorKind::InvalidData, "Invalid UTF-8 sequence"))
+                }
+            },
+            Err(e) => Err(e)
+        }
+    }
+    pub fn read_mpint(&mut self) -> io::Result<BigNum> {
+        match self.read_string() {
+            Ok(data) => {
+                if data.len() != 0 && data[0] & 0x80 != 0 {
+                    return Err(io::Error::new(io::ErrorKind::InvalidData, "Negative Big Number"));
+                }
+                if (data.len() > MAX_BIGNUM + 1) || (data.len() == MAX_BIGNUM + 1 && data[0] != 0) {
+                    return Err(io::Error::new(io::ErrorKind::InvalidData, "Big Number Too Long"));
+                }
+                // Remove Leading zeros
+                let mut i = 0;
+                let mut iter = data.iter();
+                while let Some(0) = iter.next() {
+                    i += 1;
+                }
+                match BigNum::from_slice(&data[i..]) {
+                    Ok(bn) => Ok(bn),
+                    Err(_) => Err(io::Error::new(io::ErrorKind::InvalidData, "Invalid Big Number"))
+                }
+            },
+            Err(e) => Err(e)
+        }
+    }
+    pub fn read_list(&mut self) -> io::Result<Vec<String>> {
+        match self.read_utf8() {
+            Ok(string) => {
+                Ok(string.split(',').map(String::from).collect())
+            },
+            Err(e) => Err(e)
+        }
+    }
+}
