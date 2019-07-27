681190a5f6368f15ae73bf30bc446ec5a5efdb7e
Leo <leo881003@gmail.com>
2019-07-27 16:29:25 +0800

    Add re-export

--- src/lib.rs
+++ src/lib.rs
@@ -12,3 +12,10 @@ mod format;
 pub mod keys;
 /// Extension to read/write ssh data type representations defined in [RFC 4251](https://tools.ietf.org/html/rfc4251#section-5)
 pub mod sshbuf;
+
+pub use keys::Key;
+pub use keys::KeyPair;
+pub use keys::KeyType;
+pub use keys::PrivatePart;
+pub use keys::PublicKey;
+pub use keys::PublicPart;
