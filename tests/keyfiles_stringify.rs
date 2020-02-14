5c4b47bcea01cb1903df97789d89bd55862aac19
Leo <leo881003@gmail.com>
2020-02-14 12:00:33 +0800

    Add openssh private key serialization test

--- tests/keyfiles_stringify.rs
+++ tests/keyfiles_stringify.rs
@@ -1,5 +1,6 @@
 extern crate osshkeys;
 
+use osshkeys::cipher::Cipher;
 use osshkeys::keys::*;
 
 const TEST_FILE_PASS: &[u8] = b"12345678";
@@ -39,5 +40,25 @@ fn stringify_pem_encrypt() {
     let pem = keypair.serialize_pem(Some(TEST_FILE_PASS)).unwrap();
 
     let keypair2 = KeyPair::from_keystr(&pem, Some(TEST_FILE_PASS)).unwrap();
+    utils::fingerprint_assert(&keypair, &keypair2);
+}
+
+#[test]
+fn stringify_openssh() {
+    let keypair = KeyPair::generate(KeyType::RSA, 2048).unwrap();
+    let osshpriv = keypair.serialize_openssh(None, Cipher::Null).unwrap();
+
+    let keypair2 = KeyPair::from_keystr(&osshpriv, None).unwrap();
+    utils::fingerprint_assert(&keypair, &keypair2);
+}
+
+#[test]
+fn stringify_openssh_encrypt() {
+    let keypair = KeyPair::generate(KeyType::RSA, 2048).unwrap();
+    let osshpriv = keypair
+        .serialize_openssh(Some(TEST_FILE_PASS), Cipher::Aes256_Ctr)
+        .unwrap();
+
+    let keypair2 = KeyPair::from_keystr(&osshpriv, Some(TEST_FILE_PASS)).unwrap();
     utils::fingerprint_assert(&keypair, &keypair2);
 }
